28b2fcc4187a7b8b51f29b9ae08b4bd4e9a83327
Vlad Anisov <vanisov@student.sierracollege.edu>
2023-02-02 11:55:57 -0800

    my cs class assignments

---
+++ rocketship_example/RocketShip.java
@@ -0,0 +1,20 @@
+public class RocketShip {
+   public static void main(String[] args){
+      System.out.println("    /\\   ");
+      System.out.println("   /  \\  ");
+      System.out.println("  /    \\ ");
+      System.out.println("+--------+");
+      System.out.println("|        |");
+      System.out.println("|        |");
+      System.out.println("+--------+");
+      System.out.println("| United |");
+      System.out.println("| States |");
+      System.out.println("+--------+");
+      System.out.println("|        |");
+      System.out.println("|        |");
+      System.out.println("+--------+");
+      System.out.println("    /\\   ");
+      System.out.println("   /  \\  ");
+      System.out.println("  /    \\ ");
+   }
+}
---
+++ train_assignment/CarOnRailsAnisov.java
@@ -0,0 +1,23 @@
+public class CarOnRailsAnisov {
+   public static void main(String[] args){
+      System.out.println("         ~          ");
+      System.out.println("         ~~         ");
+      System.out.println("        ~~~         ");
+      System.out.println("       ~~~~~        ");
+      System.out.println("        / \\        ");
+      System.out.println("       /   \\       ");
+      System.out.println("      /     \\      ");
+      System.out.println("     /       \\                   /       \\   ");
+      System.out.println("        |"+'"'+"|                         |"+'"'+"|        ");
+      System.out.println("        |"+'"'+"|                         |"+'"'+"|        ");
+      System.out.println("________|"+'"'+"|________         ________|"+'"'+"|________");
+      System.out.println("|                  |        |                  |");
+      System.out.println("| ---          --- |        | ---          --- |");
+      System.out.println("| ---          --- |@------@| ---          --- |");
+      System.out.println("|                  |@------@|                  |");
+      System.out.println("|__________________|        |__________________|");
+      System.out.println("    CCC//////CCC    ");
+      System.out.println("    CCC"+"\\"+"\\"+"\\"+"\\"+"\\"+"\\"+"CCC    ");
+      System.out.println("    CCC//////CCC    ");
+   }
+}
