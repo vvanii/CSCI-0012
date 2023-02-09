f2d854403c69766847539844b0d123d385a90af3
vvanii <anisovlad@outlook.com>
2023-02-09 11:53:22 -0800

    finished cookbook assignment / need to fill out comments

--- cookbook_assignment/CookBookAnisov.java
+++ cookbook_assignment/CookBookAnisov.java
@@ -21,34 +21,44 @@ public class CookBookAnisov   //<------- chnage the name to a proper name and in
    */
    public static void draw()
    {
+      DisplayCC();
+      DisplayOO();
+      DisplayOO();
+      DisplayKK();
+      DisplayBB();
+      DisplayOO();
+      DisplayOO();
+      DisplayKK();
    }
    
    /*
       Description: This method draws the letter C
    */
-   public static void m1()    //<------ must change the name m1 to a proper name
+   public static void DisplayCC()    //<------ must change the name m1 to a proper name
    {
+      System.out.println("CCCCCCCCC         CCCCCCCCC\nCC                CC\nCC                CC\nCC                CC\nCCCCCCCCC         CCCCCCCCC");
    }
    
    /* 
    Description: This method dispays the letter O
    */
    
-   public static void m2()   // <------ must change the name m2 to a proper name
+   public static void DisplayOO()   // <------ must change the name m2 to a proper name
    {
-          }
+      System.out.println(" OOOOOOOOO         OOOOOOOOO \nO          O      O          O\nO          O      O          O\nO          O      O          O\n OOOOOOOOO         OOOOOOOOO  ");
+   }
    /*
    Description: This method displays the letter K
    */
-   public static void m3()  // <------ must change the name m3 to a proper name
+   public static void DisplayKK()  // <------ must change the name m3 to a proper name
    {
-              
-      
+      System.out.println("K    K             K    K\nK   K              K   K\nK  K               K  K\nK K                K K\nK  K               K  K\nK   K              K   K\nK    K             K    K");
    }
    /*
    Description: This method displays tthe letter B
    */
-   public static void m4()    // <------ must change the name m3 to a proper name
+   public static void DisplayBB()    // <------ must change the name m3 to a proper name
    {
+      System.out.println("BBBBBBB            BBBBBBB\nB     B            B     B\nB     B            B     B\nB    B             B    B\nBBBB               BBBB\nB    B             B    B\nB     B            B     B\nB     B            B     B\nBBBBBBB            BBBBBBB");
    }
 }
