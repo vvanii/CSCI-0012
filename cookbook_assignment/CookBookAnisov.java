63341ab13270f800153482ad59e9c7d806f72d9a
vvanii <anisovlad@outlook.com>
2023-02-09 11:59:10 -0800

    added comments and self-grade

--- cookbook_assignment/CookBookAnisov.java
+++ cookbook_assignment/CookBookAnisov.java
@@ -1,25 +1,21 @@
 /*
 Name: Vlad Anisov
 Date: 02/09/2023
-Program description:
+Program description: Prints in big text letters the word "COOKBOOK"
 
-self-grade: Must explain the reasons for the given grade
-
-Must change th name of  all the given methods otherwise you will lose points
+self-grade: A, as I provided comments throughout the code and what it does, and the end result displays exactly as to what the prompt displayed
 */
-public class CookBookAnisov   //<------- chnage the name to a proper name and include your last name
+public class CookBookAnisov   // my general class for the entire cookbook
 {
-   public static void main(String[] args)
+   public static void main(String[] args) // this is the main method that displays draw() which holds all the letters
    {
       System.out.println("----------------------------------------");
       System.out.println("This program displays the word COOKBOOK in the following format");
       System.out.println("----------------------------------------\n\n");
       draw();
    }
-   /*
-     Description : This method calls all the other methods to generate the provided output
-   */
-   public static void draw()
+   
+   public static void draw() // this class contains all the letters in their order
    {
       DisplayCC();
       DisplayOO();
@@ -31,10 +27,12 @@ public class CookBookAnisov   //<------- chnage the name to a proper name and in
       DisplayKK();
    }
    
+   // This section of the code is the letters in their own methods.
+
    /*
       Description: This method draws the letter C
    */
-   public static void DisplayCC()    //<------ must change the name m1 to a proper name
+   public static void DisplayCC()    
    {
       System.out.println("CCCCCCCCC         CCCCCCCCC\nCC                CC\nCC                CC\nCC                CC\nCCCCCCCCC         CCCCCCCCC");
    }
@@ -43,21 +41,21 @@ public class CookBookAnisov   //<------- chnage the name to a proper name and in
    Description: This method dispays the letter O
    */
    
-   public static void DisplayOO()   // <------ must change the name m2 to a proper name
+   public static void DisplayOO()   
    {
       System.out.println(" OOOOOOOOO         OOOOOOOOO \nO          O      O          O\nO          O      O          O\nO          O      O          O\n OOOOOOOOO         OOOOOOOOO  ");
    }
    /*
    Description: This method displays the letter K
    */
-   public static void DisplayKK()  // <------ must change the name m3 to a proper name
+   public static void DisplayKK()  
    {
       System.out.println("K    K             K    K\nK   K              K   K\nK  K               K  K\nK K                K K\nK  K               K  K\nK   K              K   K\nK    K             K    K");
    }
    /*
    Description: This method displays tthe letter B
    */
-   public static void DisplayBB()    // <------ must change the name m3 to a proper name
+   public static void DisplayBB()    
    {
       System.out.println("BBBBBBB            BBBBBBB\nB     B            B     B\nB     B            B     B\nB    B             B    B\nBBBB               BBBB\nB    B             B    B\nB     B            B     B\nB     B            B     B\nBBBBBBB            BBBBBBB");
    }
