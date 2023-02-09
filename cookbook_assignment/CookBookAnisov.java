b67611690c951c5782145a3057c8cb2929ffd9a5
Vlad Anisov <vanisov@student.sierracollege.edu>
2023-02-09 11:20:42 -0800

    CSCI 12 course

---
+++ cookbook_assignment/CookBookAnisov.java
@@ -0,0 +1,53 @@
+/*
+Name: 
+Date:
+Program description
+self-grade: Must explain the reasons for the given grade
+
+Must change th name of  all the given methods otherwise you will loose points
+*/
+public class DisplayLastName   //<------- chnage the name to a proper name and include your last name
+{
+   public static void main(String[] args)
+   {
+      System.out.println("----------------------------------------");
+      System.out.println("This program displays the word COOKBOOK in the following format");
+      System.out.println("----------------------------------------\n\n");
+      draw();
+   }
+   /*
+     Description : This method calls all the other methods to generate the provided output
+   */
+   public static void draw()
+   {
+   }
+   
+   /*
+      Description: This method draws the letter C
+   */
+   public static void m1()    //<------ must change the name m1 to a proper name
+   {
+   }
+   
+   /* 
+   Description: This method dispays the letter O
+   */
+   
+   public static void m2()   // <------ must change the name m2 to a proper name
+   {
+          }
+   /*
+   Description: This method displays the letter K
+   */
+   public static void m3()  // <------ must change the name m3 to a proper name
+   {
+              
+      
+   }
+   /*
+   Description: This method displays tthe letter B
+   */
+   public static void m4()    // <------ must change the name m3 to a proper name
+   {
+   }
+}
