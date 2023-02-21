b1f6abae18839155c8e5fa760a8aecc43d311fef
Vlad Anisov <100395661+vvanii@users.noreply.github.com>
2023-02-20 20:28:21 -0800

    added galon conversion assignment starter code

---
+++ gallon_conversion_assignment/ChooseYourName.java
@@ -0,0 +1,34 @@
+/*Name:
+date:
+Description of what this program does
+self grade:  
+*/
+public class ChooseYourName  // <------ must change this name and then save it with the new name
+{
+  //declare the required class constant here 
+   
+   
+   public static void main (String[] args)
+   {
+     // your code, call methods #1 and #2
+            
+   }
+   ////////////////////////////////////////////////////////////
+   /* Explain what the method #1 does as a comment here*/
+   public static void method1()//<-- rename to a meaningful name
+   {
+     //your code for the method #1
+   } 
+   
+    
+   
+   ////////////////////////////////////////////////////////////////////
+   /*provide a description of the method #2*/
+   public static void method2()  //  <-- must change the method name to a meaningful name
+   {
+     //Your code for the method #2
+   }
+      
+           
+     
+  }   
