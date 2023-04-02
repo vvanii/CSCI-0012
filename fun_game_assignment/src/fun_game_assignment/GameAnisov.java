674e8530c7a8e267bf323abb17b2daccd2baee04
Vlad Anisov <100395661+vvanii@users.noreply.github.com>
2023-04-02 02:05:42 -0700

    finished game assignment

--- fun_game_assignment/src/fun_game_assignment/GameAnisov.java
+++ fun_game_assignment/src/fun_game_assignment/GameAnisov.java
@@ -21,21 +21,23 @@ public class GameAnisov {
 		System.out.println(s);
 		
 		userInput = sc.nextInt();
+		sc.nextLine();
 		
 		while (userInput < 0) {
-			System.out.println("The input you've provided is invalid.");
+			System.out.println("The input you've provided is invalid. Postive integers only.");
 			System.out.println(s);
 			userInput = sc.nextInt();
+			sc.nextLine();
 		}
 		return userInput;
 	}
-	
+
 	public static void interact(Scanner sc) {
-		System.out.println("Hello, I am a computer playing a \"Guess The Number\" game with you. What would you like to call me?\n");
+		System.out.println("Hello, I am a computer playing a \"Guess The Number\" game with you. What would you like to call me?");
 		String compName = sc.nextLine();
 		System.out.println("--------------------------------------------------------------------");
 		
-		System.out.println("Wow! I really like the name " + compName + "!\nWhat is your name?\n");
+		System.out.println("Wow! I really like the name " + compName + "!\nWhat is your name?");
 		String playerName = sc.nextLine();
 		
 		System.out.println("Hey " + playerName + ", I'm excited to play the guessing game with you!\nI will think of a number between a low and a high value that you will enter, which I will allow you to guess until you get it.");
@@ -56,6 +58,8 @@ public class GameAnisov {
 		System.out.println("Overall Results");
 		System.out.println("\tTotal Games: " + numGames);
 		System.out.println("\tTotal Number of Guesses: " + totalGuesses);
+		System.out.println("------------------------------------");
+		System.out.println("Thank you for playing!");
 	}
 	
 	public static int funGame (Scanner console, int min, int max) {
@@ -66,13 +70,15 @@ public class GameAnisov {
 		int userGuess = 0;
 		int numOfGuesses = 0;
 		
-		console.next();
-		
 		System.out.println("I am thinking of a number between " + min + " and " + max + "...");
-		console.nextLine();
-		
-		
-		
+		System.out.println("Press Enter key to continue...");
+		try
+		{
+		System.in.read();
+		}
+		catch(Exception e)
+		{}
+
 		while(userGuess != compNum) {
 			numOfGuesses++;
 			System.out.println("Enter your guess: ");
@@ -82,7 +88,7 @@ public class GameAnisov {
 			
 			if (result == "Match") {
 				System.out.println("******* Congratulations! *******");
-				System.out.println("You guessed my number in: " + numOfGuesses);
+				System.out.println("You guessed my number in: " + numOfGuesses + " guesses!");
 			} else {
 				System.out.println(result);
 			}	
@@ -99,33 +105,42 @@ public class GameAnisov {
 		String repeat = "yes";
 		
 		while (repeat.equalsIgnoreCase("yes")) {
-		String s = "Enter a low value: ";
-		String s1 = "Enter a high value: ";
+			
+			
+			String s = "Enter a minimum value: ";
+			String s1 = "Enter a maximum value: ";
 		
-		//initializing min and max values according to user input
-		min = prompt(console, s);
-		max = prompt(console, s1);
-		
-		//validating min and max values to ensure proper dynamics of the program.
-		while (max < min) {
-			System.out.println("You entered an invalid value for max. Max must be greater than min. Lets start all over again\n");
+			//initializing min and max values according to user input
+			
 			min = prompt(console, s);
 			max = prompt(console, s1);
+			
+			//validating min and max values to ensure proper dynamics of the program.
+			while (max < min) {
+				System.out.println("You entered an invalid value for max. Max must be greater than min. Lets start all over again\n");
+				min = prompt(console, s);
+				max = prompt(console, s1);
+			}
+		
+			int guesses = funGame(console, min, max);
+		
+			totalGuesses = guesses + totalGuesses;
+		
+			numOfGames++;
+		
+			System.out.println("Play again? Type \"yes\", otherwise you can type \"no\"");	
+			
+			while (!repeat.equalsIgnoreCase("yes") && !repeat.equalsIgnoreCase("no")) {
+	            System.out.println("Invalid input. Please type \"yes\" or \"no\".");
+	            repeat = console.next();
+	        }
+		
+			repeat = console.next();
+		
 		}
 		
-		int guesses = funGame(console, min, max);
-		
-		totalGuesses = guesses + totalGuesses;
-		
-		numOfGames++;
-		
-		System.out.println("Play again? Type \"yes\", otherwise you can type \"no\"\n");
-		
-		repeat = console.nextLine();
-		
-		}
 		report(numOfGames, totalGuesses);
-		
 	}
+
 
 }
