093dff46c71c97a794d1f80f3c8ea58dd82a09b1
Vlad Anisov <100395661+vvanii@users.noreply.github.com>
2023-04-14 00:15:35 -0700

    added comments to mountain assignment

--- mountain_assignment/src/ClimbAnisov.java
+++ mountain_assignment/src/ClimbAnisov.java
@@ -6,15 +6,23 @@
 import java.util.*;
 
 public class ClimbAnisov {
+
+    // this is the main method that calls the execute method
     public static void main(String[] args) {
         Random rand = new Random();
         Scanner sc = new Scanner(System.in);
         execute(rand, sc);
     }
 
+    // this method holds all the logic for the climbing simulation and is called in
+    // the main method
     public static void execute(Random rand, Scanner sc) {
+        // this boolean is used to determine if the user wants to simulate another climb
         boolean repeat = true;
         description();
+
+        // this loop will continue to ask the user if they want to climb again until
+        // they enter 'n'
         while (repeat) {
 
             int height = input(sc);
@@ -34,10 +42,14 @@ public class ClimbAnisov {
         System.out.println("\nGood Bye");
     }
 
+    // this method takes in the scanner object for user input on the height of the
+    // mountain and returns it.
     public static int input(Scanner sc) {
         boolean valid = false;
         int height = 0;
 
+        // this loop will continue to ask the user for a valid input until they enter a
+        // positive integer
         do {
             System.out.print("\nEnter the height of the mountain: ");
 
@@ -60,6 +72,8 @@ public class ClimbAnisov {
         return height;
     };
 
+    // this method takes in the height of the mountain and the random object for the
+    // random number generation. It returns the total number of slips the user took.
     public static int climb(int height, Random rand) {
         int distance = 0;
         int totalSlips = 0;
@@ -67,6 +81,7 @@ public class ClimbAnisov {
         do {
             int chance = rand.nextInt(2);
 
+            // 0 is slip, 1 is climb
             if (chance == 1) {
 
                 int steps = rand.nextInt(11) + 20;
@@ -92,6 +107,7 @@ public class ClimbAnisov {
         return totalSlips;
     }
 
+    // this method prints out the description of the program
     public static void description() {
         System.out.println(
                 "This program simulates a person climbing a mountain.\nYou define the height, and the program will simulate the climb.\nYou have a 50% chance of either climbing or slipping.\nIf the person slips, they will slip back a random number of steps.\nIf the person climbs, they will climb a random number of steps.\nThe program will continue until the person reaches the top of the mountain.");
