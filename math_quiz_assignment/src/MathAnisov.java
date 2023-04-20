9c5fe9d58b1d82b7dd3dd354d8708deb62284aa2
Vlad Anisov <100395661+vvanii@users.noreply.github.com>
2023-04-20 01:41:46 -0700

    boilerplate code for assignment

---
+++ math_quiz_assignment/src/MathAnisov.java
@@ -0,0 +1,30 @@
+/*
+Name: Vlad Anisov
+Date: 04/19/2023
+Description: //TODO: Add description
+Self-Grade: //TODO: Add self-grade
+*/
+
+import java.util.*;
+
+public class MathAnisov {
+    public static void main(String[] args) throws Exception {
+        prep();
+    }
+
+    public static void prep() {
+
+    }
+
+    public static void populate() {
+
+    }
+
+    public static int calculate() {
+        return 0;
+    }
+
+    public static int test(String[] questions, int[] answers, int[] values) {
+        return 0;
+    }
+}
