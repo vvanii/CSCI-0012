dd25e48518224b4556fa9ece6171cab418ebf7ee
Vlad Anisov <100395661+vvanii@users.noreply.github.com>
2023-05-05 02:40:13 -0700

    testing nvim

--- salary_assignment/src/SaleStudentAnisov.java
+++ salary_assignment/src/SaleStudentAnisov.java
@@ -4,42 +4,13 @@ public class SaleStudentAnisov {
 class SaleRep {
 
     private double startingSalary;
-    private double commissionRate;
     private String fullName;
     private double sales;
+    private double commissionRate;
 
-    public SaleRep(double salary, double commissionRate, String fullName) {
-
-    }
-
-    public double payCheck() {
-        return 0;
-    }
-
-    public void addSale(double newSale) {
-
-    }
-
-    public void returnItem(double amountReturned) {
-
-    }
-
-    public String toString() {
-        String s = " ";
-        s = s + "\nFull name: " + fullName + "| ";
-        s = s + "Starting salary: " + startingSalary + "| ";
-        s = s + "Commission rate: " + commissionRate + " | ";
-        s = s + "Sales amount: " + sales + "| ";
-        s = s + "pay check: " + payCheck();
-        s = s + "\nThe commission you made: " + getCommission();
-        s = s + "\nTotal paid this month: " + payCheck();
-        return s;
-    }
-
-    // setter methods
     public void setSalary(double newSalary) {
 
-        this.startingSalary = newSalary;
+        this.salary = newSalary;
 
     }
 
@@ -58,15 +29,19 @@ class SaleRep {
     }
 
     public void getRaise(double raiseRate) {
-
+        
+        return raiseRate;
     }
 
     public double getCommissionRate() {
+
+        commission = sales * commission / 100
+
         return 0;
     }
 
     public double getCommission() {
-        return 0;
+        return commission;
     }
 
     public String getFullName() {
